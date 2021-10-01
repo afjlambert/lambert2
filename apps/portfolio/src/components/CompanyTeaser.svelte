4e2ab4d84b94e93174b96b715b610d714750f274
André Lambert <afj.lambert@gmail.com>
2021-10-01 11:49:39 +0200

    Update routes

--- apps/portfolio/src/components/CompanyTeaser.svelte
+++ apps/portfolio/src/components/CompanyTeaser.svelte
@@ -1,11 +1,11 @@
 <script>
   import companies from "../_companies";
   export let slug;
-  let company = companies.find(c => c.slug === slug);
+  let company = companies.find((c) => c.slug === slug);
 </script>
 
 <h2>
-  <a href="companies/{company.slug}">
+  <a href="company/{company.slug}">
     <span>{company.name}</span>
   </a>
 </h2>
--- apps/portfolio/src/routes/portfolio.svelte
+++ apps/portfolio/src/routes/portfolio.svelte
@@ -1,8 +1,39 @@
 <script>
   import LogoList from "../components/LogoList.svelte";
   import allCompanies from "../_companies";
-  export let companies = allCompanies.filter(c => c.featured);
+  export let companies = allCompanies.filter((c) => c.featured);
 </script>
+
+<svelte:head>
+  <title>Portfolio</title>
+</svelte:head>
+
+<h1>Portfolio</h1>
+
+{#each companies.filter((c) => !!c.teaser) as company}
+  <div class="teaser">
+    <a href="/companies/{company.slug}">
+      <img src={company.logo} alt="{company.name} logo" />
+      <h2>{company.name}</h2>
+    </a>
+
+    <p>
+      {company.teaser}
+      <a href="/companies/{company.slug}">Find projects...</a>
+    </p>
+  </div>
+{/each}
+
+<div class="more">
+  <h2>Click the logo's for more information</h2>
+  <LogoList>
+    {#each companies.filter((c) => !c.teaser) as company}
+      <a href="/companies/{company.slug}">
+        <img src={company.logo} alt="{company.name} logo" />
+      </a>
+    {/each}
+  </LogoList>
+</div>
 
 <style>
   .teaser {
@@ -17,35 +48,3 @@
     margin-top: 3rem;
   }
 </style>
-
-<svelte:head>
-  <title>Portfolio</title>
-</svelte:head>
-
-<h1>Portfolio</h1>
-
-{#each companies.filter(c => !!c.teaser) as company}
-  <div class="teaser">
-    <a href="/companies/{company.slug}">
-      <img src={company.logo} alt="{company.name} logo" />
-      <h2>{company.name}</h2>
-    </a>
-
-    <p>
-      {company.teaser}
-      <a href="/companies/{company.slug}">Find projects...</a>
-    </p>
-
-  </div>
-{/each}
-
-<div class="more">
-  <h2>Click the logo's for more information</h2>
-  <LogoList>
-    {#each companies.filter(c => !c.teaser) as company}
-      <a href="/companies/{company.slug}">
-        <img src={company.logo} alt="{company.name} logo" />
-      </a>
-    {/each}
-  </LogoList>
-</div>
